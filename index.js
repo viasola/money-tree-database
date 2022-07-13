a3b79c2e6c7e6e034f010a4bfb0a31504cbad2eb
viasola <foongyuan22@gmail.com>
2022-07-13 11:06:47 +0800

    database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const db = require('./database')
+const db = require('./database/database')
 
 app.use(cors())
 app.use(express.json())
